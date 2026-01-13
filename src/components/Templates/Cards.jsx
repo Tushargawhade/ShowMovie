0d65b4b36bd786122d07ef5786aca70efff5c896
Tusshar <tushargawhade7@gmail.com>
2026-01-14 02:52:03 +0530

    trending cards

---
+++ src/components/Templates/Cards.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Cards({data , title}) {
+  return (
+
+
+
+    <div className='flex w-full flex-wrap mt-3  '>
+
+        {data.map((c,i)=>(
+
+            <Link key={i} className='w-[28vh] mr-[5%] mb-[5%]'>
+
+                <img className='shadow-[8px_17px_38px_2px_rgba(0,0,0,0.5)] h-[42vh] object-cover' src={`https://image.tmdb.org/t/p/original${ c.poste_path || c.backdrop_path}`} alt="" />
+
+
+
+                <h1 className='text-zinc-300 font-semibold  text-xl mt-2 '>
+
+                {c.original_title || c.title || c.original_name || c.name}
+                    
+                </h1>
+
+
+            
+            </Link>
+
+
+
+
+
+        ))}    
+    
+    </div>
+  )
+}
+
+export default Cards
--- src/components/Trending..jsx
+++ src/components/Trending..jsx
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import TopNav from "./Templates/TopNav";
 import Dropdown from "./Templates/Dropdown";
+import axios from "../utils/axios";
+import Cards from "./Templates/Cards";
+import Loading from "./Loading";
+
 
 function Trending() {
   const navigate = useNavigate();
 
-  return (
-    <div className="w-screen h-screen p-[2%] ">
+  const [category, setcategory] = useState("all");
+  const [duration, setduration] = useState("day");
+  const [trending, settrending] = useState([]);
+
+  const GetTrending = async () => {
+    try {
+      const { data } = await axios.get(`trending/${category}/${duration}`);
+      // console.log(data.results);
+
+      settrending(data.results);
+    } catch (error) {
+      console.log("Error :", error);
+    }
+  };
+
+  useEffect(()=>{
+    GetTrending();
+  },[category,duration])
+
+
+  console.log(trending)
+
+  return trending ? (
+    <div className="w-screen h-screen p-[2%] overflow-hidden overflow-y-auto ">
       <div className="w-full flex items-center justify-between">
+
         <h1 className="font-semibold text-2xl text-zinc-400">
           <i
             onClick={() => navigate(-1)}
@@ -20,14 +47,23 @@ function Trending() {
         <div className="flex items-center w-[80%]">
           <TopNav />
 
-          <Dropdown title="category" options={["tv", "movie", "all"]} />
+          <Dropdown title="Category" options={["tv", "movie", "all"]} func={(e)=>setcategory(e.target.value)}/>
           <div className="w-[2%]"></div>
 
-          <Dropdown title="category" options={["tv", "movie", "all"]} />
+          <Dropdown title="Duration" options={["day", "week"]} func={(e)=>setduration(e.target.value)} />
+
         </div>
+
+
       </div>
+
+        <Cards data={trending} title={category}/>
+
+
     </div>
-  );
+  ) : (   
+    <Loading/>
+ )
 }
 
 export default Trending;
