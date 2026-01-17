826409c90232fc3a4553c03df6c99356e87933ef
Tusshar <tushargawhade7@gmail.com>
2026-01-17 16:07:04 +0530

    redux-toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,34 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
-import Loading from "./components/Loading";
 import Trending from "./components/Trending.";
 import Popular from "./components/Popular";
 import Movie from "./components/Movie";
 import Tvshows from "./components/Tvshows";
 import People from "./components/People";
-
+import MovieDetails from "../src/components/MovieDetails";
+import TvDetails from "../src/components/TvDetails";
+import PersonDetails from "../src/components/PersonDetail";
 
 const App = () => {
   return (
     <div className="w-screeen h-screen bg-[#1F1E24] flex">
+
       <Routes>
+
         <Route path="/" element={<Home />} />
         <Route path="/trending" element={<Trending />} />
         <Route path="/popular" element={<Popular />} />
         <Route path="/movie" element={<Movie />} />
+        <Route path="/movie/details/:id" element={<MovieDetails />} />
         <Route path="/tv" element={<Tvshows />} />
+        <Route path="/tv/details/:id" element={<TvDetails />} />
         <Route path="/people" element={<People />} />
-
+        <Route path="/people/details/:id" element={<PersonDetails />} />
 
       </Routes>
+
+
     </div>
   );
 };
